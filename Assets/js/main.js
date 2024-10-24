2f4428bbb4f4772903431677ae0d19f2b951ddf9
jlunia7792 <jlunia7792@conestogac.on.ca>
2024-10-24 19:57:35 -0400

    adding assets folder

---
+++ Assets/js/main.js
@@ -0,0 +1,252 @@
+const data = {
+    "sports": {
+        "football": {
+            'image': "fa-futbol",
+            "country_specific_leagues": {
+                "england": { "name": "Premier League", "flag": "flags/england.png", "logo": "logos/premier_league.png" },
+                "spain": { "name": "La Liga", "flag": "flags/spain.png", "logo": "logos/la_liga.png" },
+                "germany": { "name": "Bundesliga", "flag": "flags/germany.png", "logo": "logos/bundesliga.png" },
+                "italy": { "name": "Serie A", "flag": "flags/italy.png", "logo": "logos/serie_a.png" },
+                "france": { "name": "Ligue 1", "flag": "flags/france.png", "logo": "logos/ligue_1.png" },
+                "portugal": { "name": "Primeira Liga", "flag": "flags/portugal.png", "logo": "logos/primeira_liga.png" },
+                "netherlands": { "name": "Eredivisie", "flag": "flags/netherlands.png", "logo": "logos/eredivisie.png" },
+                "belgium": { "name": "Pro League", "flag": "flags/belgium.png", "logo": "logos/pro_league.png" },
+                "scotland": { "name": "Scottish Premiership", "flag": "flags/scotland.png", "logo": "logos/scottish_premiership.png" },
+                "turkey": { "name": "Süper Lig", "flag": "flags/turkey.png", "logo": "logos/super_lig.png" },
+                "russia": { "name": "Russian Premier League", "flag": "flags/russia.png", "logo": "logos/russian_premier_league.png" },
+                "sweden": { "name": "Allsvenskan", "flag": "flags/sweden.png", "logo": "logos/allsvenskan.png" },
+                "austria": { "name": "Austrian Bundesliga", "flag": "flags/austria.png", "logo": "logos/austrian_bundesliga.png" },
+                "denmark": { "name": "Danish Superliga", "flag": "flags/denmark.png", "logo": "logos/danish_superliga.png" },
+                "greece": { "name": "Super League Greece", "flag": "flags/greece.png", "logo": "logos/super_league_greece.png" },
+                "switzerland": { "name": "Swiss Super League", "flag": "flags/switzerland.png", "logo": "logos/swiss_super_league.png" },
+                "ukraine": { "name": "Ukrainian Premier League", "flag": "flags/ukraine.png", "logo": "logos/ukrainian_premier_league.png" },
+                "romania": { "name": "Liga I", "flag": "flags/romania.png", "logo": "logos/liga_i.png" },
+                "serbia": { "name": "Serbian SuperLiga", "flag": "flags/serbia.png", "logo": "logos/serbian_superliga.png" },
+                "norway": { "name": "Eliteserien", "flag": "flags/norway.png", "logo": "logos/eliteserien.png" },
+                "finland": { "name": "Veikkausliiga", "flag": "flags/finland.png", "logo": "logos/veikkausliiga.png" },
+                "israel": { "name": "Israeli Premier League", "flag": "flags/israel.png", "logo": "logos/israeli_premier_league.png" },
+                "czech_republic": { "name": "Czech First League", "flag": "flags/czech_republic.png", "logo": "logos/czech_first_league.png" }
+            }
+        },
+        "cricket": {
+            "image": "fa-baseball-ball",
+            "country_specific_leagues": {
+                "india": { "name": "Indian Premier League (IPL)", "flag": "flags/india.png", "logo": "logos/ipl.png" },
+                "australia": { "name": "Big Bash League (BBL)", "flag": "flags/australia.png", "logo": "logos/bbl.png" },
+                "england": { "name": "Vitality T20 Blast", "flag": "flags/england.png", "logo": "logos/t20_blast.png" },
+                "west_indies": { "name": "Caribbean Premier League (CPL)", "flag": "flags/west_indies.png", "logo": "logos/cpl.png" },
+                "south_africa": { "name": "Mzansi Super League (MSL)", "flag": "flags/south_africa.png", "logo": "logos/msl.png" },
+                "new_zealand": { "name": "Super Smash", "flag": "flags/new_zealand.png", "logo": "logos/super_smash.png" },
+                "pakistan": { "name": "Pakistan Super League (PSL)", "flag": "flags/pakistan.png", "logo": "logos/psl.png" },
+                "bangladesh": { "name": "Bangladesh Premier League (BPL)", "flag": "flags/bangladesh.png", "logo": "logos/bpl.png" },
+                "ireland": { "name": "Ireland Inter-Provincial Series", "flag": "flags/ireland.png", "logo": "logos/ip.png" }
+            }
+        },
+        "basketball": {
+            "image": "fa-basketball-ball",
+            "country_specific_leagues": {
+                "usa": { "name": "NBA", "flag": "flags/usa.png", "logo": "logos/nba.png" },
+                "spain": { "name": "Liga ACB", "flag": "flags/spain.png", "logo": "logos/liga_acb.png" },
+                "turkey": { "name": "Basketball Super League", "flag": "flags/turkey.png", "logo": "logos/bsl.png" },
+                "france": { "name": "LNB Pro A", "flag": "flags/france.png", "logo": "logos/lnb.png" },
+                "germany": { "name": "Basketball Bundesliga", "flag": "flags/germany.png", "logo": "logos/basketball_bundesliga.png" },
+                "italy": { "name": "Lega Basket Serie A", "flag": "flags/italy.png", "logo": "logos/lega_basket.png" },
+                "australia": { "name": "NBL", "flag": "flags/australia.png", "logo": "logos/nbl.png" },
+                "philippines": { "name": "PBA", "flag": "flags/philippines.png", "logo": "logos/pba.png" },
+                "japan": { "name": "B.League", "flag": "flags/japan.png", "logo": "logos/b_league.png" },
+                "greece": { "name": "Greek Basket League", "flag": "flags/greece.png", "logo": "logos/greek_basket_league.png" },
+                "lithuania": { "name": "LKL", "flag": "flags/lithuania.png", "logo": "logos/lkl.png" }
+            }
+        },
+        "hockey": {
+            "image": "fa-hockey-puck",
+            "country_specific_leagues": {
+                "usa": { "name": "NHL", "flag": "flags/usa.png", "logo": "logos/nhl.png" },
+                "canada": { "name": "OHL", "flag": "flags/canada.png", "logo": "logos/ohl.png" },
+                "russia": { "name": "KHL", "flag": "flags/russia.png", "logo": "logos/khl.png" },
+                "sweden": { "name": "SHL", "flag": "flags/sweden.png", "logo": "logos/shl.png" },
+                "finland": { "name": "Liiga", "flag": "flags/finland.png", "logo": "logos/liiga.png" },
+                "czech_republic": { "name": "Czech Extraliga", "flag": "flags/czech_republic.png", "logo": "logos/czech_extraliga.png" },
+                "germany": { "name": "DEL", "flag": "flags/germany.png", "logo": "logos/del.png" },
+                "switzerland": { "name": "NL", "flag": "flags/switzerland.png", "logo": "logos/nl.png" }
+            }
+        },
+        "tennis": {
+            "image": "fa-table-tennis",
+            "country_specific_leagues": {
+                "usa": { "name": "USTA", "flag": "flags/usa.png", "logo": "logos/usta.png" },
+                "france": { "name": "French Open", "flag": "flags/france.png", "logo": "logos/french_open.png" },
+                "australia": { "name": "Australian Open", "flag": "flags/australia.png", "logo": "logos/australian_open.png" },
+                "uk": { "name": "Wimbledon", "flag": "flags/uk.png", "logo": "logos/wimbledon.png" },
+                "canada": { "name": "Canadian Open", "flag": "flags/canada.png", "logo": "logos/canadian_open.png" },
+                "italy": { "name": "Italian Open", "flag": "flags/italy.png", "logo": "logos/italian_open.png" },
+                "germany": { "name": "German Open", "flag": "flags/germany.png", "logo": "logos/german_open.png" }
+            }
+        },
+        "rugby": {
+            "image": "fa-football-ball",
+            "country_specific_leagues": {
+                "england": { "name": "English Premiership", "flag": "flags/england.png", "logo": "logos/english_premiership.png" },
+                "france": { "name": "Top 14", "flag": "flags/france.png", "logo": "logos/top_14.png" },
+                "australia": { "name": "Super Rugby", "flag": "flags/australia.png", "logo": "logos/super_rugby.png" },
+                "new_zealand": { "name": "Mitre 10 Cup", "flag": "flags/new_zealand.png", "logo": "logos/mitre_10_cup.png" },
+                "south_africa": { "name": "Currie Cup", "flag": "flags/south_africa.png", "logo": "logos/currie_cup.png" },
+                "wales": { "name": "Welsh Premier Division", "flag": "flags/wales.png", "logo": "logos/welsh_premier_division.png" },
+                "scotland": { "name": "Scottish Premiership", "flag": "flags/scotland.png", "logo": "logos/scottish_premiership.png" }
+            }
+        },
+        "baseball": {
+            "image": "fa-baseball-ball",
+            "country_specific_leagues": {
+                "usa": { "name": "MLB", "flag": "flags/usa.png", "logo": "logos/mlb.png" },
+                "japan": { "name": "NPB", "flag": "flags/japan.png", "logo": "logos/npb.png" },
+                "south_korea": { "name": "KBO League", "flag": "flags/south_korea.png", "logo": "logos/kbo.png" },
+                "mexico": { "name": "LMB", "flag": "flags/mexico.png", "logo": "logos/lmb.png" },
+                "puerto_rico": { "name": "LBPRC", "flag": "flags/puerto_rico.png", "logo": "logos/lbprc.png" }
+            }
+        }
+    }
+};
+
+$(document).ready(function () {
+    populateSports();
+    displayAllLeagues();
+    checkFavorites();
+
+
+    // Populate the sports list
+    function populateSports() {
+        const sportsList = $('#sports-list');
+        for (const sport in data.sports) {
+            sportsList.append(`
+                    <li  class="sport-item" data-sport='${sport}'>
+                        <span class="sport-name"><i class="fas ${data.sports[sport].image}"></i> ${sport.charAt(0).toUpperCase() + sport.slice(1)}</span>
+            <i class="far fa-heart heart-icon"></i>
+                    </li>
+                `);
+        }
+    }
+
+    // Populate the league list for a specific sport
+    function populateLeagues(sport) {
+        const leagueList = $("#league-list");
+        leagueList.empty();
+        let leagueCount = 0;
+        for (let country in data.sports[sport].country_specific_leagues) {
+            const league = data.sports[sport].country_specific_leagues[country];
+            leagueList.append(`
+                    <div class="col league-card p-2">
+                        <div class="card">
+                            <img src="${league.logo}" class="card-img-top" alt="${league.name} Logo">
+                            <div class="card-body bg-dark text-white">
+                                <h5 class="card-title">${league.name}</h5>
+                                <p class="card-text">${country.charAt(0).toUpperCase() + country.replace(/_/g, ' ').slice(1)}</p>
+                            </div>
+                        </div>
+                    </div>
+                `);
+            leagueCount++;
+        }
+        $("#total-count").text(`Total Leagues: ${leagueCount}`);
+    }
+
+    // Display all leagues by default
+    function displayAllLeagues() {
+        const leagueList = $("#league-list");
+        leagueList.empty();
+        let leagueCount = 0;
+
+        for (let sport in data.sports) {
+            for (let country in data.sports[sport].country_specific_leagues) {
+                const league = data.sports[sport].country_specific_leagues[country];
+                leagueList.append(`
+                        <div class="col league-card p-2">
+                            <div class="card">
+                                <img src="${league.logo}" class="card-img-top" alt="${league.name} Logo">
+                                <div class="card-body bg-dark text-white">
+                                    <h5 class="card-title">${league.name}</h5>
+                                    <p class="card-text">${country.charAt(0).toUpperCase() + country.replace(/_/g, ' ').slice(1)}</p>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+                leagueCount++;
+            }
+        }
+        $("#total-count").text(`Total Leagues: ${leagueCount}`);
+    }
+
+    // Filter sports list based on search input
+    $('#search').on('input', function () {
+        const searchTerm = $(this).val().toLowerCase();
+        $('#sports-list li').each(function () {
+            const sportName = $(this).data('sport').toLowerCase();
+            $(this).toggle(sportName.includes(searchTerm));
+        });
+    });
+
+    // Event listener for sports selection
+    $(document).on("click", ".sport-item", function () {
+        const sport = $(this).data("sport");
+        populateLeagues(sport);
+
+    });
+
+    // Search functionality for leagues
+    $("#search-league").on("keyup", function () {
+        const searchTerm = $(this).val().toLowerCase();
+        $(".league-card").filter(function () {
+            $(this).toggle($(this).find("h5").text().toLowerCase().includes(searchTerm));
+        });
+        const visibleCount = $(".league-card:visible").length;
+        $("#total-count").text(`Total Leagues: ${visibleCount}`);
+    });
+
+    $('.sport-item').on('dblclick', function () {
+        const heartIcon = $(this).find('.heart-icon');
+        const sportName = $(this).data('sport');
+
+        if (heartIcon.hasClass('far')) {
+            // If it's currently not selected (unfilled heart)
+            heartIcon.removeClass('far').addClass('fas'); // Make the heart filled
+            addToFavorites(sportName);
+        } else {
+            // If it's currently selected (filled heart)
+            heartIcon.removeClass('fas').addClass('far'); // Make the heart unfilled
+            removeFromFavorites(sportName);
+
+        }
+        checkFavorites();
+    });
+
+    // Function to add selected sport to favorites list
+    function addToFavorites(sportName) {
+        const favoriteItem = $('<li></li>')
+            .addClass('favorite-sport sport-item')
+            .attr('data-sport', sportName)
+            .text(sportName)
+            .prepend(`<i class="fas ${data.sports[sportName].image} pe-1"></i>`);
+
+        $('#favorite-sports-container').append(favoriteItem);
+    }
+
+    // Function to remove deselected sport from favorites list
+    function removeFromFavorites(sportName) {
+        $(`.favorite-sport[data-sport="${sportName}"]`).remove();
+    }
+
+    function checkFavorites() {
+        const favoritesCount = $('.favorite-sport').length;
+
+        if (favoritesCount === 0) {
+            // If no favorites, show the "Select your favorite sport" message
+            $('.no-favorites').show();
+        } else {
+            // If favorites exist, hide the message
+            $('.no-favorites').hide();
+        }
+    }
+
+    // Initially check if there are any favorites selected
+
+});
